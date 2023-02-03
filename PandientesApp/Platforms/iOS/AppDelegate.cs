d2c01562ad288526001e859a8d6870c54fa5188f
StigmaQueen <71120596+StigmaQueen@users.noreply.github.com>
2023-02-03 16:10:00 -0600

    MauiApp

---
+++ PandientesApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PandientesApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
