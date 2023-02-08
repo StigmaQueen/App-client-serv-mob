67231f7ac0f41e0384652f4866c7cfec61935807
StigmaQueen <71120596+StigmaQueen@users.noreply.github.com>
2023-02-08 14:52:20 -0600

    Insert-services, get lista, errors

--- PandientesApp/Services/PendientesServices.cs
+++ PandientesApp/Services/PendientesServices.cs
@@ -16,13 +16,13 @@ namespace PandientesApp.Services
         {
             cliet = new HttpClient()
             {
-                BaseAddress = new Uri("https://pendintes.sistemas19.com")
+                BaseAddress = new Uri("https://pendientes.sistemas19.com/")
             };
 
         }
         public async Task<List<Actividad>> GetAll()
         {
-            var result = await cliet.GetAsync("api/pendientes");//peticion regresa un response
+            HttpResponseMessage result = await cliet.GetAsync("api/pendientes");//peticion regresa un response
             if(result.IsSuccessStatusCode)//verificar si la respuesta fue existosa
             {
                 var json= await result.Content.ReadAsStringAsync();
@@ -30,6 +30,18 @@ namespace PandientesApp.Services
                 return lista;
             }
             return new List<Actividad>();
+        }
+        public async Task Insert(Actividad a)
+        {
+            var json = JsonConvert.SerializeObject(a);
+            StringContent scontent = new StringContent(json, Encoding.UTF8, "application/json");
+            var result = await cliet.PostAsync("api/pendientes",scontent);
+            if (!result.IsSuccessStatusCode)
+            {
+                json = await result.Content.ReadAsStringAsync();
+                string s = JsonConvert.DeserializeObject<string>(json);
+                throw new Exception($"Ha ocurrido un error: {result.StatusCode}\n{s}");
+            }
         }
     }
 }
--- PandientesApp/ViewModels/PendientesViewModel.cs
+++ PandientesApp/ViewModels/PendientesViewModel.cs
@@ -5,30 +5,47 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PandientesApp.ViewModels
 {
-    public class PendientesViewModel : INotifyCollectionChanged
+    public class PendientesViewModel : INotifyPropertyChanged
     {
         PendientesServices ServicePendientes;
-        public ObservableCollection<Actividad> Actividades { get; set; }
+        public ObservableCollection<Actividad> Actividades { get; set; } = new ObservableCollection<Actividad>();
+        public string Error { get; set; }
         public PendientesViewModel()
         {
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
             ServicePendientes= new PendientesServices();
-            CargarDatos();
+             _= CargarDatos();
+            
         }
 
-        async void CargarDatos()
+        private async void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            await CargarDatos();
+        }
+
+        async Task CargarDatos()
         {
             if(Connectivity.Current.NetworkAccess==NetworkAccess.Internet) //Verficar si hay conexión a internet
             {
+                var lista= await ServicePendientes.GetAll();
                 Actividades.Clear();
+                lista.ForEach(a => Actividades.Add(a));
+                Error = "";
+            }
+            else
+            {
+                Error = "No hay conexión internet";
+             
             }
         }
 
-        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
